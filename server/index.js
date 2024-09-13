1c862e667fdfee25916f1ede24bcdf38ff52df2b
Hoang Dung Le Duy <dung.le.hoang@pirago.work>
2024-09-13 16:30:14 +0700

    Initial Project

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express =require ('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(9999, () => {
+    console.log('Server started on port 9999');
+})
